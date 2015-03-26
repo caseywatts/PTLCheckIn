9b49da33e0a9b694edc2f9cd6607959eba4938e4
DerekPeaceTech <derek@peacetechlab.org>
2015-03-25 20:25:53 -0400

    implemented local storage, changed button layout
    stores variables in local storage and recalls

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,9 +15,26 @@ angular.module('starter.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.mySettings = {
-    name: 'Casey Watts',
-    message: 'lol',
-    emailTo: 'derek.gildea+caseysafe@gmail.com',
-    callTo: '123-456-7890'
+    name: window.localStorage['name'] || 'John Smith',
+    message: window.localStorage['message'] || 'This is my Default Checkin message.',
+    emailTo: window.localStorage['emailTo'] || 'checkin@awesome.com',
+    callTo: window.localStorage['callTo'] || '1234567890'
   };
+  
+  $scope.Store = function(){
+	window.localStorage['name'] = $scope.mySettings.name;
+	var userName = window.localStorage['name'];
+	
+	window.localStorage['message'] = $scope.mySettings.message;
+	var userMessage = window.localStorage['message'];
+	
+	window.localStorage['emailTo'] = $scope.mySettings.emailTo;
+	var ckemailTo = window.localStorage['emailTo'];
+	
+	window.localStorage['callTo'] = $scope.mySettings.callTo;
+	var ckcallTo = window.localStorage['callTo'];
+	
+	alert("Information Saved"); 
+  }
+  
 });
