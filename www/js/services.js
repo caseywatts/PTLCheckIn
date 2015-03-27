cdf712296892ae36038ebead79ebe2a1579dc023
caseywatts <casey.s.watts@gmail.com>
2015-03-27 05:41:19 -0400

    extract MySettings hash to its own service

--- www/js/services.js
+++ www/js/services.js
@@ -1,24 +1,10 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
+.service('MySettings', function() {
+  this.all = {
+    name: 'Casey Watts',
+    message: 'lol',
+    emailTo: 'derek.gildea+caseysafe@gmail.com',
+    callTo: '123-456-7890'
   };
 });
