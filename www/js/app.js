bdbdbdbc565a3a9bef5e22692d761167f913b63f
caseywatts <casey.s.watts@gmail.com>
2015-04-07 18:47:44 -0400

    Revert "install angular-local-storage bower package"
    This reverts commit 146a6afbe3043c916320cd6a9661dcb6fa395a23.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ionic.utils', 'LocalStorageModule', 'starter.services', 'starter.controllers'])
+angular.module('starter', ['ionic', 'ionic.utils', 'starter.services', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
