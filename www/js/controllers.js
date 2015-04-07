b9bb4cb44f8eca2906dc3952f617a3087578b671
caseywatts <casey.s.watts@gmail.com>
2015-04-07 19:07:04 -0400

    remove two unused services: ionic.utils and MySettings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, MySettings, $localStorage) {
+.controller('DashCtrl', function($scope, $localStorage) {
 //    $scope.mySettings = MySettings.getSettings();
   $scope.$storage = $localStorage;
 })
 
-.controller('AccountCtrl', function($scope, MySettings, $localStorage) {
+.controller('AccountCtrl', function($scope, $localStorage) {
   $scope.$storage = $localStorage;
 //   $scope.mySettings = MySettings.getSettings();
 //   $scope.saveMySettings = function(mySettings){
