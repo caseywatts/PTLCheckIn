5923c05d16aa72bf2c0e2a5e3898bee57a52af6b
caseywatts <casey.s.watts@gmail.com>
2015-04-07 20:39:18 -0400

    add new message button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,9 @@ angular.module('starter.controllers', [])
     {
       $scope.$storage.messages = CheckInMessages.all();
     }
+  $scope.newMessage = function() {
+    $scope.$storage.messages.push({message: "[text]"});
+  };
 })
 
 .controller('AccountCtrl', function($scope, $localStorage) {
