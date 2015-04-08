5f7e2478b61bf7b70e5140dcd5bf28ef9a8851e9
caseywatts <casey.s.watts@gmail.com>
2015-04-07 20:29:07 -0400

    remove unused code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,10 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $localStorage, CheckInMessages) {
-//    $scope.mySettings = MySettings.getSettings();
   $scope.$storage = $localStorage;
   $scope.$storage.messages = CheckInMessages.all();
 })
 
 .controller('AccountCtrl', function($scope, $localStorage) {
   $scope.$storage = $localStorage;
-//   $scope.mySettings = MySettings.getSettings();
-//   $scope.saveMySettings = function(mySettings){
-//     MySettings.setSettings(mySettings);
-//   };
 });
